a071877d8fddc3be6a4ae989c51c352dece0ba07
ozXoz <honourkorkmaz@gmail.com>
2023-07-13 17:33:32 -0400

    update backend and fix the bug

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const PORT = 3333; // Change the port number as needed
 // app.use(cors()); // Add this line
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://ecommerce-blue-eight.vercel.app',
   credentials: true, // Enable credentials (cookies, authorization headers, etc)
 }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://ecommerce-blue-eight.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
