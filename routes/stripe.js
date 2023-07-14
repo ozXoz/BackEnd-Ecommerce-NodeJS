314060463337bcde18a5a0425a4f2fb6f0a1e2f5
ozXoz <honourkorkmaz@gmail.com>
2023-07-13 21:52:07 -0400

    update API_KEY

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const API="sk_test_51N9IDbIkwxyvGO5pUOCGidHgQQTuix1Yl0o950aBxDOBdljGkib2YbN4kqBCrwQ1mjBSGutlmRh4wKhrO10kLft500KMwDNSVo";
+// const API="sk_test_51N9IDbIkwxyvGO5pUOCGidHgQQTuix1Yl0o950aBxDOBdljGkib2YbN4kqBCrwQ1mjBSGutlmRh4wKhrO10kLft500KMwDNSVo";
+const API="sk_test_51NTb7XL1whot0TadqB84tbSFtxlIb5vOfHmUFNKDnckGZbBnKb7BETzNxv2Q6z2w9hdsnYG0A1epohwsjqvQeFR400GMJvGhpF";
 const stripe = require("stripe")(API);
 
 router.post("/payment", async (req, res) => {
